b1ba9e0f80558f3f5798f95a26545c0b5a6fdd17
Daniel <marillancadaniel@gmail.com>
2022-07-06 16:23:25 -0400

    componentes base de login y registro

---
+++ src/components/OptionForm.vue
@@ -0,0 +1,44 @@
+<template>
+<div class="bg-secondary py-5">
+    <div class="container">
+        <ul class="nav nav-pills nav-justified mb-3" id="ex1" role="tablist">
+        <li class="nav-item" @click="onClick(true)">
+            <a class="nav-link active">Login</a>
+        </li>
+        <li class="nav-item" @click="onClick(false)">
+            <a class="nav-link">Registro</a>
+        </li>
+        </ul>
+        <LoginApp v-if="mostrarLogin"/>
+        <RegisterForm v-if="!mostrarLogin"/>
+    </div>
+</div>
+
+
+</template>
+
+<script>
+import LoginApp from './LoginApp.vue'
+import RegisterForm from './RegisterForm.vue'
+
+export default {
+    components: {
+        LoginApp,
+        RegisterForm
+    },
+    data(){
+        return {
+            mostrarLogin: true
+        }
+    },
+    methods: {
+        onClick(mostrar){
+            this.mostrarLogin = mostrar;
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
