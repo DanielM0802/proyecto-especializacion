d7c1e0db4caf210b7233df52798c287f1382f2e3
Elian Bustamante <e.bustamante02@ufromail.cl>
2022-07-09 21:13:17 -0400

    Se agregan dependencias axios y chartjs, se agrega grafico en componente SesionEstudio

--- src/components/LoginApp.vue
+++ src/components/LoginApp.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="mask d-flex align-items-center h-100 py-5">
+  <div class="mask d-flex align-items-center h-100 py-5" v-if="acciones.mostrarLoginForm">
     <div class="container h-100">
       <div class="row d-flex justify-content-center align-items-center h-100">
         <div class="col-12 col-md-9 col-lg-7 col-xl-6">
@@ -8,8 +8,8 @@
               <h2 class="text-uppercase text-center mb-5">Inicia sesión</h2>
               <form>
                 <div class="form-outline mb-4">
-                  <input type="email" id="form3Example3cg" class="form-control form-control-lg" />
-                  <label class="form-label" for="form3Example3cg">Tu ID de usuario</label>
+                  <input type="text" id="username" name="username" class="form-control form-control-lg" v-model="username" required/>
+                  <label class="form-label" for="username" >Nombre de usuario</label>
                 </div>
                 <div class="form-outline mb-4">
                   <input type="password" id="form3Example4cg" class="form-control form-control-lg" />
@@ -17,7 +17,7 @@
                 </div>
                 <div class="d-flex justify-content-center">
                   <button type="button"
-                    class="btn btn-success btn-block btn-lg gradient-custom-4 text-body">Iniciar Sesión</button>
+                    class="btn btn-success btn-block btn-lg gradient-custom-4 text-body" @click="login('mostrarSesionEstudio', username)">Iniciar Sesión</button>
                 </div>
               </form>
             </div>
@@ -26,11 +26,55 @@
       </div>
     </div>
   </div>
+  <div>
+    <SesionEstudio v-if="acciones.mostrarSesionEstudio"/>
+  </div>
 </template>
 
 <script>
-export default {
+import axios from 'axios';
+import SesionEstudio from './SesionEstudio.vue';
 
+export default {
+  name: 'LoginApp',
+  components: {
+    SesionEstudio
+  },
+  data() {
+    return {
+      acciones: {
+        mostrarLoginForm: true,
+        mostrarSesionEstudio: false
+      },
+      username: null,
+      ultimaAccion: 'mostrarLoginForm',
+      users: []
+    }
+  },
+  methods: {
+    login(accion, username) {
+
+      this.mounted();
+      
+      for (let index = 0; index < this.users.length; index++) {
+        
+        if (this.users[index].username.localeCompare(username)) {
+          console.log(username);
+        }
+        
+      }
+
+      this.acciones[this.ultimaAccion] = false;
+      this.acciones[accion] = true;
+      this.ultimaAccion = accion;
+    },
+    async mounted() {
+      await axios.get(`https://7qak3a37b4dh7kisebhllubdxq0dnehm.lambda-url.us-east-1.on.aws/`).then((respuesta) => {
+        this.users = respuesta.data;
+      })
+      console.log(this.users[0])
+    }
+  }
 }
 </script>
 
--- src/components/SesionEstudio.vue
+++ src/components/SesionEstudio.vue
@@ -1,11 +1,49 @@
 <template>
-  <h1>{{sesion.username}}</h1>
+    <div>
+
+        <h2>Bienvenidx estimadx {{sesion.username}}!</h2>
+        <div class="sesion-listado-container">
+
+            <h4 class="list-title">Sesiones realizadas</h4>
+            <div class="card">
+                <table class="table tabla-sesiones" aria-describedby="tabla-sesiones">
+                <thead>
+                    <tr>
+                        <th id="id_sesion">ID</th>
+                        <th id="fecha_sesion">FECHA</th>
+                        <th id="puntaje_sesion">PUNTAJE</th>
+                        <th id="tiempo_sesion">TIEMPO (minutos)</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr class="list-row-hover" v-for="session in sesion.sessions" :key="session.id">
+                        <th :id="session.id">{{session.id}}</th>
+                        <td>{{session.date}}</td>
+                        <td>{{session.score}}</td>
+                        <td>{{session.duration}}</td>
+                    </tr>
+                </tbody>
+            </table>
+            </div>
+        </div>
+
+        <!--GRAFICO CHARTJS-->
+        <div class="grafico card mt-3">
+            <canvas id="myChart"></canvas>
+        </div>
+        <!--GRAFICO CHARTJS-->
+
+    </div>
 </template>
 
 <script>
+import Chart from 'chart.js/auto';
+
+
 export default {
-    data(){
-        return{
+    data() {
+        return {
+            graficoSesiones: null,
             sesion : 
                 {
                 id: 1,
@@ -74,10 +112,46 @@ export default {
                 ]
                 },
         }
+    },
+    async mounted() {
+        const ctx = document.getElementById('myChart');
+        const labels = [];
+       
+        this.sesion.sessions.forEach(sesionActual => {
+            labels.push('Sesion '.concat(sesionActual.id))
+        });
+
+        const data = {
+            labels: labels,
+            datasets: [{
+                label: 'Rendimiento (puntajes)',
+                backgroundColor: 'rgb(30,144,255)',
+                borderColor: 'rgb(30,144,255)',
+                data: this.sesion.sessions.map(sesionActual => sesionActual.score)
+            },
+            {
+                label: 'Tiempo (minutos)',
+                backgroundColor: 'rgb(255, 51, 51)',
+                borderColor: 'rgb(255, 51, 51)',
+                data: this.sesion.sessions.map(sesionActual => sesionActual.duration)
+            }]
+        };
+        const config = {
+            type: 'line',
+            data: data,
+            options: {
+                responsive: true
+            }
+        };
+
+        this.graficoSesiones = new Chart(ctx, config);
     }
 }
 </script>
 
-<style>
-
+<style scoped>
+.grafico {
+    width: 1000px;
+    height: 530px;
+}
 </style>
