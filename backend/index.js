c9789df14ee7c0fd6d43066ffc65129e34885df9
Daniel <marillancadaniel@gmail.com>
2022-07-12 14:11:03 -0400

    modelos, rutas y coneccion a base de datos

---
+++ backend/index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+const app = express();
+
+const mongoose = require('mongoose');
+
+// database
+
+const uri = 'mongodb://DanielM_0802:r2Fl7bOhsM0Wul82@ac-pxribpk-shard-00-00.rof0thq.mongodb.net:27017,ac-pxribpk-shard-00-01.rof0thq.mongodb.net:27017,ac-pxribpk-shard-00-02.rof0thq.mongodb.net:27017/?ssl=true&replicaSet=atlas-lums8h-shard-0&authSource=admin&retryWrites=true&w=majority';
+
+
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log('MongoDB Connected…')
+  })
+  .catch(err => console.log(err))
+
+
+// settings
+app.set('port', process.env.PORT || 3000);
+
+// middlewares
+app.use(morgan('dev'));
+app.use(express.json());
+
+// routes
+app.use('/api/users', require('./routes/users'));
+
+// static
+app.use(express.static(path.join(__dirname, 'public')));;
+
+// listenning on port
+app.listen(app.get('port'), () => {
+  console.log(`server on port ${app.get('port')}`);
+});
