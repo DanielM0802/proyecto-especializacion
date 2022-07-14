10a6cfff688c3d4650f32fecab0c46962ed4b3dd
Elian Bustamante <e.bustamante02@ufromail.cl>
2022-07-14 01:42:51 -0400

    Agregar dependencia axios backend y agregar logica en sessions.js

--- backend/routes/sessions.js
+++ backend/routes/sessions.js
@@ -1,7 +1,25 @@
 const express = require('express');
+const axios = require ('axios');
+const User = require('../models/User');
 const router = express.Router();
 
 router.get('/:id', async (req, res) => {
+    const users = [];
+    const sesiones = [];
+    const user = await User.findById(req.params.id);
+    const username = user.username;
+
+    await axios.get(`https://7qak3a37b4dh7kisebhllubdxq0dnehm.lambda-url.us-east-1.on.aws/`).then((respuesta) => {
+      users = respuesta.data;
+    })
+
+    for (let index = 0; index < users.length; index++) {
+        
+        if (users[index].username === username) {
+            sesiones = users[index].sessions;
+            break;
+        } 
+    }
     //const user = await User.findOne({ username: username });
     //const username = user.username
 
@@ -13,7 +31,7 @@ router.get('/:id', async (req, res) => {
         error: null,
         data: {
             title: "sesiones del usuario",
-            //sessions: sesiones
+            sessions: sesiones
         }
     })
 })
